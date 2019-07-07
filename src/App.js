bdbb6d1f28a94105267768376a3ad0ce091e9eb3
lekpramual <lek.pramual@gmail.com>
2019-07-07 20:11:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1
+          Learn React 12345
         </a>
       </header>
     </div>
